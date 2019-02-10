24459805beaf19eee4508c71eab5cafab0219095
peymanmohebbi <47505065+peymanmohebbi@users.noreply.github.com>
2019-02-10 22:37:39 +0330

    adding files

---
+++ Number wizard UI/Number wizard UI/Assets/scripts/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guesstxt;
+
+    
+    int guess;
+
+
+	// Use this for initialization
+	void Start () {
+
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        Nextguess();
+        
+    }
+
+    void Nextguess()
+    {
+        guess = Random.Range(min, max);
+        guesstxt.text = guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess+1;
+        Nextguess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        Nextguess();
+    }
+
+}
---
+++ Number wizard UI/Number wizard UI/Assets/scripts/nextscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextscene : MonoBehaviour {
+
+	public void Loadnextscece()
+    {
+        int Currentsceneindx = SceneManager.GetActiveScene().buildIndex;
+        if (Currentsceneindx != 2)
+            SceneManager.LoadScene(Currentsceneindx + 1);
+        else
+            SceneManager.LoadScene(Currentsceneindx - 2);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
